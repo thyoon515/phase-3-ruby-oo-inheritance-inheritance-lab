f1fad199dba44215a564ce1ece85169c08394838
thyoon515 <thyoon515@gmail.com>
2022-08-22 20:47:46 -0400

    commit tree 49185308fceb07419135a3a82f5d85c2ca9a4e25

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+require 'pry'
+
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 
 end
